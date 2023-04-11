686388d0c2082b23ff26fe72bfe3d658048f3dd7
aziffarhan <aziffarhan1947@gmail.com>
2023-04-11 14:51:28 +0800

    Login system

--- quiz3.js
+++ quiz3.js
@@ -2,12 +2,20 @@ const express = require('express')
 const app = express()
 const port = 1947
 
-
 app.use(express.json())
-
 app.post('/login', (req, res) => {
     console.log(req.body)
-    res.send('Login')
+
+    let result = login(req.body.username, req.body.password)
+    res.send(result)
+})
+
+app.use(express.json())
+app.post('/register', (req, res) => {
+  console.log(req.body)
+
+  let result = register(req.body.username, req.body.password, req.body.name, req.body.email)
+  res.send(result)
 })
 
 app.get('/', (req, res) => {
@@ -18,10 +26,50 @@ app.get('/bye', (req, res) => {
   res.send('Bye bye UTeM!')
 })
 
-app.post('/register', (req, res) => {
-  res.send('Account Created!')
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+function login(reqUsername, reqPassword){
+  let matchUser = dbUsers.find(user => user.username == reqUsername)
+  if(!matchUser) return "User not found!"
+  if(matchUser.password == reqPassword){
+      return matchUser
+  }
+  else
+  {
+      return "Invalid password!"
+  }
+}
+
+function register(reqUsername, reqPassword, reqName, reqEmail){
+  dbUsers.push({
+      username: reqUsername,
+      password: reqPassword,
+      name: reqName,
+      email: reqEmail
+  })
+}
+
+let dbUsers = [
+  {
+      username: "azif",
+      password: "123456",
+      name: "Azif",
+      email: "aziffarhan1947@gmail.com"
+  },
+  {
+      username: "ayam",
+      password: "biskutlutut",
+      name: "Ayam",
+      email: "ayamguring@gmail.com"
+  },
+  {
+      username: "abu",
+      password: "abudabest",
+      name: "Abu",
+      email: "abudabest@gmail.com"
+  }
+]
